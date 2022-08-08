4718dd7caffb964268b2a2c223e33dade34ffeab
sacrifice0726 <83894027+sacrifice0726@users.noreply.github.com>
2022-08-08 19:43:07 +0800

    Add files via upload
    智慧語音箱

--- NLP/Program/NLPProgram/torch_dataloader_type.py
+++ NLP/Program/NLPProgram/torch_dataloader_type.py
@@ -4,7 +4,6 @@ import torch
 from parameters_dict import load_parameters_dict
 from bilstm_model import classify_LSTM
 
-# pkl_path = os.path.abspath('Data/data_pkl')+'/'
 pkl_path = os.path.abspath('NLP/Data/data_pkl/type_data_pkl')+'/'
 
 #   create load data by pickle 
@@ -20,11 +19,6 @@ def load_data(dataset):
         return  label_to_id,id_to_label,word_to_id,id_to_word
 
 def choose_dataset():
-    # print("使用 complete_new.pkl 請按 1")
-    # print("使用 hostel_new.pkl 請按 2")
-    # print("使用 KTV_new.pkl 請按 3")
-    # print("使用 restaurant_new.pkl 請按 4")
-    # inp = print(input("請輸入要使用的數據集："))
     inp = 1
     if inp == 1:
         dataset = "complete.pkl"
@@ -40,7 +34,6 @@ def choose_dataset():
         
     return dataset
 
-print(choose_dataset())
 
 label_to_id,id_to_label,word_to_id,id_to_word,\
 =load_data(choose_dataset())
@@ -48,27 +41,17 @@ label_to_id,id_to_label,word_to_id,id_to_word,\
 number_tag_dic,embedding_dic,hidden_dic,dropout_dic,lr_dic,weight_decay_dic,\
     max_epoch_dic,batch_size_dic,dic_num_dic=load_parameters_dict(choose_dataset(),0)
 
-#   set torch parameter (embedding_size,hidden_size,dorpout ....)
 class Config:
     embedding_dim = embedding_dic
     hidden_dim = hidden_dic
     vocab_size = len(word_to_id)
-    # num_tags = len(label_to_id)
     num_tags = 6
-
     dropout = dropout_dic
     lr = lr_dic
     weight_decay = weight_decay_dic
     
 def create_dataloader():
-    
-    #   craete torch_dataloader , device and train_parameter
     device = torch.device('cpu')
-    max_epoch = max_epoch_dic
-    batch_size = batch_size_dic
-    dic_num = dic_num_dic
-    # batch_size = 1  # To test
-       
     config = Config()
     model = classify_LSTM(config).to(device)
     return device, model
